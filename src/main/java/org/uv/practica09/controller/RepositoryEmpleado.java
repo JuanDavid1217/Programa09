5277e273632663bbe1b49b2be98ec5e8517a381b
juan <juand_david2001@hotmail.com>
2023-05-14 23:12:43 -0600

    Se corrigen issues

---
+++ src/main/java/org/uv/practica09/controller/RepositoryEmpleado.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.uv.practica09.controller;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author juand
+ */
+public interface RepositoryEmpleado extends CrudRepository<Empleado, Long>{
+    
+}
